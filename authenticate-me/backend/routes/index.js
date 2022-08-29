b100c1d533ee4c7b5d048d9fdbd2bc60c7545367
Kevin Longboy <kevinlongboy@gmail.com>
2022-08-29 03:05:45 -0700

    Added error-handling middleware for common errors and debugging.

--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
 
 /************************* TEST ROUTES *************************/
 // router.get('/hello/world', function (req, res) {
