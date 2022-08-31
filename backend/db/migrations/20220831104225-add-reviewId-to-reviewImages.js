dfca3a0dab1c6c021c84e09e302d1c9f5a3e476a
Kevin Longboy <kevinlongboy@gmail.com>
2022-08-31 03:46:25 -0700

    Established assocaition between ReviewImage and Review models/tables.

---
+++ backend/db/migrations/20220831104225-add-reviewId-to-reviewImages.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn(
+      'ReviewImages',
+      'reviewId',
+      {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'Reviews' },
+      }
+    )
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn(
+      'ReviewImages',
+      'reviewId',
+    )
+  }
+};
--- backend/db/models/reviewimage.js
+++ backend/db/models/reviewimage.js
@@ -11,9 +11,14 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      ReviewImage.belongsTo(models.Review, { foreignKey: 'reviewId' });
     }
   }
   ReviewImage.init({
+    reviewId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
     url: {
       type: DataTypes.STRING,
       allowNull: false,
