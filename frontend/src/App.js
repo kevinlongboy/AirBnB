c4f640e8b95620e1a87c33caa7ea038033d822e5
Kevin Longboy <kevinlongboy@gmail.com>
2022-09-19 02:33:44 -0700

    Completed 'SignupFormPage component' for Phase 2.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
+import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
 
 function App() {
@@ -13,9 +14,11 @@ function App() {
 
   return isLoaded && (
     <Switch>
-      <h1>Hello from App!</h1>
       <Route path="/login">
         <LoginFormPage />
+      </Route>
+      <Route path="/signup">
+        <SignupFormPage />
       </Route>
     </Switch>
   );
