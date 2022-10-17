5608fc2eb484c7afca4ec6405cc40242288051e5
Kevin Longboy <kevinlongboy@gmail.com>
2022-10-17 14:12:17 -0700

    Formatting.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,37 @@
+/******************************** IMPORTS ********************************/
+// libraries
 import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import SignUpForm from "./components/Navigation/NoSession/SignUpForm";
-
+// local files
 import * as sessionActions from "./store/sessionReducer";
-import AllSpots from "./components/Spots/AllSpots";
-import UserSpots from "./components/Spots/UserSpots";
-import SpotPage from "./components/Spots/SpotPage";
-import SpotEdit from "./components/Spots/UpdateSpotForm";
-import CreateSpotForm from "./components/Spots/CreateSpotForm";
-import Reviews from "./components/Reviews/UserReviews"
-import ReviewEdit from "./components/Reviews/UpdateReviewForm";
-
 import Navigation from "./components/Navigation";
+import AllSpots from "./components/Spots/AllSpots";
+import SpotPage from "./components/Spots/SpotPage";
+import UserSpots from "./components/Spots/UserSpots";
+import CreateSpotForm from "./components/Spots/CreateSpotForm";
+import UpdateSpotForm from "./components/Spots/UpdateSpotForm";
+import UserReviews from "./components/Reviews/UserReviews"
+import UpdateReviewForm from "./components/Reviews/UpdateReviewForm";
 import Footer from "./components/Footer/index.js"
+// import Host from "./components/Host/Host"; // yeet
 
-import Host from "./components/Host/Host"; // yeet
 
+/******************************* COMPONENT *******************************/
 function App() {
+
+  /************ reducer/API communication ************/
   const dispatch = useDispatch();
+
+  /****************** manage state *******************/
   const [isLoaded, setIsLoaded] = useState(false);
+
   useEffect(() => {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
   }, [dispatch]);
 
+  /**************** render component *****************/
   return (
     <>
     <Navigation isLoaded={isLoaded} path="/"/>
@@ -44,7 +52,7 @@ function App() {
           </Route>
 
           <Route exact path={'/spots/:spotId/edit'}>
-            <SpotEdit />
+            <UpdateSpotForm />
           </Route>
 
           <Route exact path={'/hosting'}>
@@ -52,21 +60,21 @@ function App() {
           </Route>
 
           <Route exact path={'/reviews'}>
-            <Reviews />
+            <UserReviews />
           </Route>
 
           <Route exact path={'/reviews/:reviewId/edit'}>
-            <ReviewEdit />
+            <UpdateReviewForm />
           </Route>
 
 
-          <Route exact path={'/legacy/host'}>
+          {/* <Route exact path={'/legacy/host'}>
             <Host />
           </Route>
 
          <Route exact path={"/legacy/signup"}>
             <SignUpForm />
-          </Route>
+          </Route> */}
 
         </Switch>
       )}
@@ -76,4 +84,6 @@ function App() {
   );
 }
 
+
+/******************************** EXPORTS ********************************/
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,18 @@
+/******************************** IMPORTS ********************************/
+// libraries
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import { ModalProvider } from "./context/Modal";
-
+import { Provider } from "react-redux";
+// local files
 import configureStore from "./store";
-import { restoreCSRF, csrfFetch } from "./store/csrf";
 import * as sessionActions from "./store/sessionReducer";
+import { restoreCSRF, csrfFetch } from "./store/csrf";
+import { ModalProvider } from "./context/Modal";
+import App from "./App";
+import "./index.css";
 
+/************* conditional components **************/
 const store = configureStore();
 
 if (process.env.NODE_ENV !== "production") {
@@ -20,7 +23,11 @@ if (process.env.NODE_ENV !== "production") {
   window.sessionActions = sessionActions;
 }
 
+
+/******************************* COMPONENT *******************************/
 function Root() {
+
+  /**************** render component *****************/
   return (
     <Provider store={store}>
       <ModalProvider>
